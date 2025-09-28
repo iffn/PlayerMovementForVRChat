8f6742151fec481b8698539c9493ce3a8eba6063
iffn <iffn1990@gmail.com>
2025-09-28 21:24:25 +0200

    Implemented stations

---
+++ General/EnterStationOnClick.cs
@@ -0,0 +1,16 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+
+public class EnterStationOnClick : UdonSharpBehaviour
+{
+    [SerializeField] VRCStation linkedStation;
+
+    public override void Interact()
+    {
+        linkedStation.UseStation(Networking.LocalPlayer);
+    }
+}
