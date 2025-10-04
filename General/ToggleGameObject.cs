06e9faae5db2a0df73ed3d733a37a9dcf5a64778
iffn <iffn1990@gmail.com>
2025-10-04 12:23:32 +0200

    Implemented climbing system toggle

---
+++ General/ToggleGameObject.cs
@@ -0,0 +1,22 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ToggleGameObject : UdonSharpBehaviour
+{
+    [SerializeField] GameObject[] linkedObjects;
+
+    public override void Interact()
+    {
+        base.Interact();
+
+        foreach(GameObject go in linkedObjects)
+        {
+            go.SetActive(!go.activeSelf);
+
+            Debug.Log($"{go.name}: {go.activeSelf}");
+        }
+    }
+}
