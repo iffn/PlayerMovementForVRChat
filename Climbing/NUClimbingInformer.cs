4e9bc730e892bee39ea24c9beae7a791be3d5fad
iffn <iffn1990@gmail.com>
2025-10-04 11:42:04 +0200

    Started linking climbing to NUMovement

---
+++ Climbing/NUClimbingInformer.cs
@@ -0,0 +1,21 @@
+﻿
+using Nessie.Udon.Movement;
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class NUClimbingInformer : ClimbingActivationInformer
+{
+    [SerializeField] NUMovement linkedNUMovement;
+
+    public override void ClimbingStart()
+    {
+        linkedNUMovement._ControllerDisable();
+    }
+
+    public override void ClimbingStop()
+    {
+        linkedNUMovement._ControllerEnable();
+    }
+}
